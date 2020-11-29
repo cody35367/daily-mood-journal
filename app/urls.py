81e97726a370bf948975fe0236c40b1530cac7dc
Cody Hodges <cody35367@gmail.com>
2020-11-29 01:00:56 -0600

    Got basic login

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(), name='logout'),
+    path('create/', views.create, name='create'),
+    path('<int:journal_id>/edit/', views.edit, name='edit'),
+    path('<int:journal_id>/read/', views.read, name='read'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+from .models import Journal
+
+@login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    journal_list = Journal.objects.order_by('-create_date')
+    context = {'page_title': "Home", 'journal_list': journal_list}
+    return render(request, 'index.html', context)
+
+@login_required
+def create(request):
+    context = {'page_title': "Create"}
+    return render(request, 'crud.html', context)
+
+@login_required
+def edit(request,journal_id):
+    context = {'page_title': "Edit", 'journal_id': journal_id}
+    return render(request, 'crud.html', context)
+
+@login_required
+def read(request,journal_id):
+    context = {'page_title': "Read", 'journal_id': journal_id}
+    return render(request, 'crud.html', context)
